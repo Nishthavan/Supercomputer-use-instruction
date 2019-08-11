b15ebff6f05539d50b6cce4c9c28c071441c2319
Nishthavan <48491021+Nishthavan@users.noreply.github.com>
2019-08-11 08:48:32 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,10 +1,13 @@
 NV_GPU=GPU-8bfe8998-2454-ea30-f9cd-0d6b1c8abf08 nvidia-docker run -it -v /home/dgxuser107/:/home/dgxuser107/data nvcr.io/nvidia/tensorflow:17.11
 cd ..
 cd home
-cd dgxuser107/
+cd dgxuser/
 cd data/ 
-pip install matplotlib
-pip install pandas
-pip install keras
-pip install --upgrade keras==2.1.3
-pip install pillow
+apt-get install python3-pip 
+python3 -m pip install --upgrade pip
+pip3 install matplotlib
+pip3 install pandas
+pip3 install keras
+pip3 install --upgrade keras==2.1.3
+pip3 install pillow
+
